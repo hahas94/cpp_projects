dae4785370e23de8feafb372fb7a1e6ff5d8b597
hahas94 <harady.hasan@gmail.com>
2024-07-29 21:06:15 +0200

    added test for empty  print function.

--- cpp_projects/structures/linked_list_test.cpp
+++ cpp_projects/structures/linked_list_test.cpp
@@ -1,8 +1,16 @@
 #include "linked_list.hpp"
 #include "../../test/catch.hpp"
+#include <sstream>
 
 TEST_CASE("Empty list"){
 	List list;
+}
+
+TEST_CASE("Test empty print"){
+	List list;
+	std::ostringstream oss{};
+	list.print(oss);
+	REQUIRE(oss.str() == "[]");
 }
 
 
