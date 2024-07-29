c222294f0981a79b848b6d595e860366105b67b2
hahas94 <harady.hasan@gmail.com>
2024-07-29 20:16:57 +0200

    defining an empty linked list.

---
+++ cpp_projects/structures/linked_list.cpp
@@ -0,0 +1,33 @@
+/**
+ * linked_list.cpp
+ * ---------------
+ * Description: 
+ * 	
+ * 	----- Linked List Library -----
+ * 
+ * 	The task is to write a library that will be used by other programmers.
+ * 	The library should implement an object for representing a singly linked  
+ * 	list data structure, where the list is always sorted. Functionality such
+ * 	as insert, remove, iterations, deep-copy constructor, copy assignment,
+ * 	move constructor, move assignment and destructors are included. 
+ * 	Part of the task is to handle dynamic memory and pointers, without 
+ * 	any memory leak.
+ * 
+ * 	A linked list data structure consists of a number of nodes, where each
+ * 	node holds some data and a pointer to the next node in the list. As such
+ * 	the list only keeps track of the first node.
+ * 	
+ * 	The code must be developed using TDD - Test-Driven Development. The Catch2 
+ * 	library is used for the tesing part.
+ * 
+ * 	*/
+
+#include "linked_list.hpp"
+
+List::List()
+	: first{nullptr}
+	{}
+
+
+
+// ============== END OF FILE ==============
