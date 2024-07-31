5a01f1375cfa2db004bc0ac3344796365a8d0b2f
hahas94 <harady.hasan@gmail.com>
2024-07-31 18:50:16 +0200

    adding tests for insert method.

--- cpp_projects/structures/linked_list_test.cpp
+++ cpp_projects/structures/linked_list_test.cpp
@@ -13,6 +13,31 @@ TEST_CASE("Test empty print"){
 	REQUIRE(oss.str() == "[]");
 }
 
+TEST_CASE("Test insert method"){
+	List list;
+	std::ostringstream oss{};
+	
+	// first node
+	list.insert(2);
+	
+	// new first node
+	list.insert(0);
+
+	// between two nodes
+	list.insert(1);
+
+	// last nodes
+	list.insert(3);
+
+	// equal nodes
+	list.insert(0);
+	list.insert(1);
+	list.insert(3);
+
+	list.print(oss);
+	REQUIRE(oss.str() == "[0, 0, 1, 1, 2, 3, 3]");
+}
+
 
 
 // ============== END OF FILE ==============
