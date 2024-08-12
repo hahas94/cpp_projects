8a1d8e1b254e3c64ba30b753a6b69e895720730c
hahas94 <harady.hasan@gmail.com>
2024-08-12 19:25:37 +0200

    Refactoring the simulate function to use methods from the Circuit class instead of implementing it standalone.

--- cpp_projects/simulator/simulate.cpp
+++ cpp_projects/simulator/simulate.cpp
@@ -1,41 +1,84 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <iomanip> 
 #include "circuit.hpp"
 
 void simulate(){
 	int steps{200000};
-	int lines{10};
+	int num_lines_to_print{10};
+	int print_step{steps / num_lines_to_print};
 	Component::time_step = 0.01;
 	
-	std::vector<Component*> circuit;
+	{
+		Circuit circuit{};
+		
+		Connection P, N, R23, R124;
 	
-	Connection P, N, R23, R124;
-
-	circuit.push_back(new Battery("B1", 24, P, N));
-	circuit.push_back(new Resistor("R1", 6, P, R124));
-	circuit.push_back(new Resistor("R2", 4, R124, R23));
-	circuit.push_back(new Resistor("R3", 8, R23, N));
-	circuit.push_back(new Resistor("R4", 12, R124, N));
-
-	int lines_to_print{steps / lines};
-
+		circuit.add_component(new Battery("Bat", 24, P, N));
+		circuit.add_component(new Resistor("R1", 6, P, R124));
+		circuit.add_component(new Resistor("R2", 4, R124, R23));
+		circuit.add_component(new Resistor("R3", 8, R23, N));
+		circuit.add_component(new Resistor("R4", 12, R124, N));
 	
-	
-	
+		circuit.print_titles();
 
-	std::cout << std::fixed << std::setprecision(2);
+		for(int step = 0; step < steps; step++){
+			circuit.step();
 
-	for(int step = 0; step < steps; step++){
-		for(Component* comp : circuit){
-			comp->step();
-			if(step % lines_to_print == 0){
-				std::cout << std::setw(6) << comp->get_voltage() << std::setw(6) << comp->get_current(); 
+			if((step+1) % print_step == 0){
+				circuit.step_print();
+				std::cout << std::endl;
 			}
 		}
-		if(step % lines_to_print == 0){
-			std::cout << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	{
+		Circuit circuit{};
+		
+		Connection P, N, L, R;
+
+		circuit.add_component(new Battery("Bat", 24, P, N));
+		circuit.add_component(new Resistor("R1", 150, P, L));
+		circuit.add_component(new Resistor("R2", 50, P, R));
+		circuit.add_component(new Resistor("R3", 100, L, R));
+		circuit.add_component(new Resistor("R4", 300, L, N));
+		circuit.add_component(new Resistor("R5", 250, R, N));
+	
+		circuit.print_titles();
+
+		for(int step = 0; step < steps; step++){
+			circuit.step();
+
+			if((step+1) % print_step == 0){
+				circuit.step_print();
+				std::cout << std::endl;
+			}
+		}
+	}
+
+	std::cout << std::endl;
+
+	{
+		Circuit circuit{};
+		
+		Connection P, N, L, R;
+
+		circuit.add_component(new Battery("Bat", 24, P, N));
+		circuit.add_component(new Resistor("R1", 150, P, L));
+		circuit.add_component(new Resistor("R2", 50, P, R));
+		circuit.add_component(new Capacitor("C3", 1.0, L, R));
+		circuit.add_component(new Resistor("R4", 300, L, N));
+		circuit.add_component(new Capacitor("C5", 0.75, R, N));
+	
+		circuit.print_titles();
+
+		for(int step = 0; step < steps; step++){
+			circuit.step();
+
+			if((step+1) % print_step == 0){
+				circuit.step_print();
+				std::cout << std::endl;
+			}
 		}
 	}
 }
